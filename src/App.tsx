a3f82b27fa9aa969ac2a14f1244398f4de59d58d
Joachim Birkelund Fredheim <joachibf@stud.ntnu.no>
2023-09-20 18:27:46 +0200

    Resolve "localstorage persistance of type filtering"

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import Pokeprofile from "./components/Pokeprofile.tsx";
 import Pokegrid from "./components/Pokegrid.tsx";
 import Navbar from "./components/Navbar.tsx";
-import { Pokemon, PokemonType } from "./components/types.tsx";
+import {Pokemon, PokemonType} from "./components/types.tsx";
+
 
 const App: React.FC = () => {
-  const [activeType, setActiveType] = useState<PokemonType>({ name: "fire" });
-  const [activePokemon, setActivePokemon] = useState<Pokemon>({
-    name: "charmander",
-  });
 
-  useEffect(() => {}, [activeType, activePokemon]);
+    const getTypeFromSessionStorage = () => {
+        const current_value = sessionStorage.getItem("type");
+        if (current_value) {
+            return {name: current_value}
+        } else {
+            return {name: "water"}
+        }
+    }
+    const [activeType, setActiveType] = useState<PokemonType>(() => getTypeFromSessionStorage());
+    const [activePokemon, setActivePokemon] = useState<Pokemon>({
+        name: "charmander",
+    });
 
-  return (
-    <>
-      <div className={"app"}>
-        <Pokeprofile pokemon={activePokemon} />
-        <Navbar filterCallback={setActiveType} />
-        <Pokegrid type={activeType.name} selectCallback={setActivePokemon} />
-      </div>
-    </>
-  );
+    useEffect(() => {
+        sessionStorage.setItem("type", activeType.name) //this will run every time the user does anything. Might not be optimal
+    }, [activeType, activePokemon]);
+
+    return (
+        <>
+            <div className={"app"}>
+                <Pokeprofile pokemon={activePokemon}/>
+                <Navbar selectedType={activeType} filterCallback={setActiveType}/>
+                <Pokegrid type={activeType.name} selectCallback={setActivePokemon}/>
+            </div>
+        </>
+    );
 };
 
 export default App;
